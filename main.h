e3d13e783fd6be567db676e2babe74adef06e227
Stephen Kariuki <stephenkariuki838@gmail.com>
2023-10-17 04:21:47 -0700

    corrected syntax

--- main.h
+++ main.h
@@ -50,7 +50,6 @@ int print_string(va_list types, char buffer[],
 int print_percent(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
-/* Functions to print numbers */
 int print_int(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -63,34 +62,18 @@ int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_hexa_upper(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-
-
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-
-
 int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-
-
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
-
-
-
 int print_reverse(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
-
-
-
 int print_rot13string(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
